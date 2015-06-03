0fe933c2d6be35309d86ebc0db1c74ae11dc0d1e
Nazareno <nazareno.anselmi@gmail.com>
2015-06-02 23:33:59 -0300

    Ultima version

--- md5/md5.c
+++ md5/md5.c
@@ -1,38 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdint.h>
 #include <string.h>
-#include <openssl/md5.h>  //PARA USAR LAS FUNCIONES DE MD5 DE OPENSSL HAY QUE INSTALAR
-						  //sudo apt-get update
-						  //sudo apt-get install libssl-dev --fix-missing
 
-//const char *string = "The quick brown fox jumped over the lazy dog's back";
+int main (int argc, char**argv){
 
-int main(int argc , char *argv[]){
-  int i;
-  char leer[500000];
-  memset(leer,'\0',500000);
-  FILE *archivo;
-  unsigned char result[MD5_DIGEST_LENGTH];
-
-  if ((archivo = fopen(argv[1],"r")) == NULL){
-	  puts("");
-	  puts("Error en Fopen");
-	  puts("");
-	  exit(1);
-  }
-  if (fread(&leer,sizeof(char),sizeof(leer), archivo) == 0){
-	puts("");
-	perror("Error en Fread");
-	puts("");
-	fclose(archivo);
-  	exit(1);
-  }
-
-  MD5(leer, strlen(leer), result);
-
-  for(i = 0; i < MD5_DIGEST_LENGTH; i++)
-    printf("%02x", result[i]);
-  printf("\n");
-
-  return EXIT_SUCCESS;
+	int fd[2];
+	int childpid;
+	pipe(fd);
+	char result[1000];
+	if (argc!=2){
+		printf ("Cantidad de argumentos insuficientes\n");
+		exit(-1);
+	}
+	memset(result,'\0',1000);
+	if ( (childpid = fork() ) == -1){
+		fprintf(stderr, "FORK failed");
+	} else if( childpid == 0) {
+		close(1);
+		dup2(fd[1], 1);
+		close(fd[0]);
+		execlp("/usr/bin/md5sum","md5sum",argv[1],NULL);
+	}
+	wait(NULL);
+	read(fd[0], result, sizeof(result));
+	printf("%s",result);
+	return 0;
 }
