87aae6d36a0ab762632148e638dcafde8946bdc8
Nazareno <nazareno.anselmi@gmail.com>
2015-06-12 13:46:39 -0300

    fd

--- test file/file.c
+++ test file/file.c
@@ -19,64 +19,60 @@
 
 int main(){
 	printf("Eligió Copiar un archivo local al MDFS\n");
-    FILE * archivoLocal;
-	char* path;
-	path = string_new();
-	char car;
-	int i=0;
-	int j=0;
-	int k=0;
-	int n=0;
-	int pos;
-	char cadena[100];
-	memset(cadena,'\0',100);
+	    FILE * archivoLocal;
+		char* path;
+		path = string_new();
+		char car;
+		int i=0;
+		int j=0;
+		int k=0;
+		int n=0;
+		int pos;
+		char cadena[100];
+		memset(cadena,'\0',100);
 
-	printf("Ingrese el path del archivo local \n");
-    scanf("%s", path);
-    //Validacion de si existe el archivo en el filesystem local
-    if((archivoLocal = fopen(path,"r"))==NULL){
-    	perror("fopen");
-    	exit(1);
-    }
-    while ((car = fgetc(archivoLocal)) != EOF)
-    {
-    	i++;
-    	k++;
-    	cadena[i-1]=car;
-    	if(car == '\n'){
-    		pos=i-1;
-    		n=k;
-    	}
-    	if(strlen(cadena) == 20){
-    		if(car == '\n'){ //Caso Feliz
+		printf("Ingrese el path del archivo local \n");
+	    scanf("%s", path);
+	    //Validacion de si existe el archivo en el filesystem local
+	    if((archivoLocal = fopen(path,"r"))==NULL){
+	    	perror("fopen");
+	    	exit(1);
+	    }
+	    while ((car = fgetc(archivoLocal)) != EOF)
+	    {
+	    	i++;
+	    	k++;
+	    	cadena[i-1]=car;
+	    	if(car == '\n'){
+	    		pos=i-1;
+	    		n=k;
+	    	}
+	    	if(strlen(cadena) == 20){
+	    		if(car == '\n'){ //Caso Feliz
 
-    			//Ordenar los bloques del archivo según el espacio disponible
-    			//Copiar el contenido del Buffer en los bloques mas vacios por triplicado
-    			//Vaciar el Buffer
-    			printf ("Cadena FULL: %s\n",cadena);
-    			pos = 0;
-    			i=0;
-    			memset(cadena,'\0',100);
-    		}else{ //Caso en que el bloque no termina en "\n"
-    			for(j=pos+1;j<100;j++) cadena[j]='\0';
-    			printf ("Cadena Con Fragmentacion: %s\n",cadena);
-    			pos = 0;
-    			i=0;
-    			fseek(archivoLocal,n,SEEK_SET);
-    			k=n;
-    			memset(cadena,'\0',100);
-    			//Ordenar los bloques del archivo según el espacio disponible
-    			//Copiar el contenido del Buffer en los bloques mas vacios por triplicado
-    			//Vaciar el Buffer
+	    			//Ordenar los bloques del archivo según el espacio disponible
+	    			//Copiar el contenido del Buffer en los bloques mas vacios por triplicado
+	    			//Vaciar el Buffer
+	    			printf ("Cadena FULL: %s\n",cadena);
+	    			pos = 0;
+	    			i=0;
+	    			memset(cadena,'\0',100);
+	    		}else{ //Caso en que el bloque no termina en "\n"
+	    			for(j=pos+1;j<100;j++) cadena[j]='\0';
+	    			printf ("Cadena Con Fragmentacion: %s\n",cadena);
+	    			pos = 0;
+	    			i=0;
+	    			fseek(archivoLocal,n,SEEK_SET);
+	    			k=n;
+	    			memset(cadena,'\0',100);
+	    			//Ordenar los bloques del archivo según el espacio disponible
+	    			//Copiar el contenido del Buffer en los bloques mas vacios por triplicado
+	    			//Vaciar el Buffer
 
-    		}
-    	}
-    }
-    /*for(j=pos+1;j<100;j++){
-    	cadena[j]='\0';
-    }*/
+	    		}
+	    	}
+	    }
+	    fclose(archivoLocal);
+	    return 0;
 
-
-    fclose(archivoLocal);
-    return 0;
 }
